e90ffa76e9979793e5dfb6267c85c9798492178f
Philip Johnson <philip@uptechstudio.com>
2025-05-16 15:02:26 -0700

    Refactor for manifest 3
    This makes a few changes to modernize the extension for manifest 3.
    
    The main thing it does is switch from callbacks to promises and async
    functions. This makes the code a little more readable.
    
    I also switched the tabs query from `currentWindow` to `lastFocusedWindow`, per the recommendation in the documentation.

--- background.js
+++ background.js
@@ -9,10 +9,14 @@ const deletionOptions = [
   'webSQL'
 ];
 
-function reloadCurrentTab() {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.update(tabs[0].id, { url: tabs[0].url });
-  });
+async function getCurrentTab() {
+  const tabs = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+  return tabs[0];
+}
+
+async function reloadCurrentTab() {
+  const activeTab = await getCurrentTab();
+  await chrome.tabs.update(activeTab.id, { url: activeTab.url });
 }
 
 function setRestingIcon() {
@@ -43,32 +47,33 @@ function getDomain(url) {
   return match && match[0];
 }
 
-const finish = (reload) => {
+async function finish(reload) {
   if (reload) {
-    reloadCurrentTab();
+    await reloadCurrentTab();
   }
 }
 
-function removeAllData(site) {
+async function removeSelectedData(site) {
   const optionIds = ['reload', ...deletionOptions];
 
-  chrome.storage.sync.get(optionIds, results => {
-    chrome.browsingData.remove({
-      origins: [site]
-    }, {
-      appcache: results.appcache,
-      cacheStorage: results.cacheStorage,
-      cookies: results.cookies,
-      indexedDB: results.indexedDB,
-      localStorage: results.localStorage,
-      pluginData: results.pluginData,
-      serviceWorkers: results.serviceWorkers,
-      webSQL: results.webSQL
-    }, () => finish(results.reload));
-  });
+  const options = await chrome.storage.sync.get(optionIds);
+  await chrome.browsingData.remove({
+    origins: [site]
+  }, {
+    appcache: options.appcache,
+    cacheStorage: options.cacheStorage,
+    cookies: options.cookies,
+    indexedDB: options.indexedDB,
+    localStorage: options.localStorage,
+    pluginData: options.pluginData,
+    serviceWorkers: options.serviceWorkers,
+    webSQL: options.webSQL
+  })
+
+  await finish(options.reload);
 }
 
-// Initialize settings on install
+// Initialize settings on initial extension installation
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({
     reload: true,
@@ -84,14 +89,11 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 // toolbar button clicked
-chrome.action.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener(async () => {
   setBusyIcon();
   setTimeout(() => setRestingIcon(), 1000);
 
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    var activeTab = tabs[0];
-
-    const domain = getDomain(activeTab.url);
-    removeAllData(domain);
-  });
+  const activeTab = await getCurrentTab();
+  const domain = await getDomain(activeTab.url);
+  await removeSelectedData(domain);
 });
